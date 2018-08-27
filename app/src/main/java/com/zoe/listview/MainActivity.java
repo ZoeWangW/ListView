77c4c27929144638be515358c19606e1bfb990d2
ZoeWangW <13438165026@163.com>
2018-08-27 21:12:19 +0800

    新建关于ListView的用法
    Signed-off-by: ZoeWangW <13438165026@163.com>

---
+++ app/src/main/java/com/zoe/listview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.zoe.listview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] data = { "Apple","Banana","Orange","Watermelon","Pear","Grape","Pineapple","Strawberry","Cheery","Mango"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity
+        .this,android.R.layout.simple_list_item_1,data);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+    }
+}
