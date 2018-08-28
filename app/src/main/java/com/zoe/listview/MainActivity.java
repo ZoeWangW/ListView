96d68f1554797116091eb173da99d0e0f06441af
ZoeWangW <13438165026@163.com>
2018-08-28 11:18:12 +0800

    添加项目点击事件
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- app/src/main/java/com/zoe/listview/MainActivity.java
+++ app/src/main/java/com/zoe/listview/MainActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +28,13 @@ public class MainActivity extends AppCompatActivity {
         FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = fruitList.get(position);
+                Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
     private void initFruits(){
         Fruit apple = new Fruit("Apple",R.mipmap.apple);
